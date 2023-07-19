395346a09ea8631c85e83d88cb5fd1f715c73db3
careules22 <carolina@legalhub.la>
2023-07-18 22:42:43 -0300

    Homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,33 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+//let numeroArr = num.split();
+let suma = 0;
+let potencia = 0;
 
-function DecimalABinario(num) {}
+
+   for (let i = num.length - 1; i >= 0; i--){
+      suma += parseInt(num.charAt(i)) * 2 ** potencia;
+      potencia++;
+   }
+return suma;
+}
+
+console.log (BinarioADecimal("111"));
+
+function DecimalABinario(num) {
+   let numBinario = "";
+
+   while (num > 0) {
+      let resto = num % 2;
+      numBinario = resto + numBinario;
+
+      num = Math.floor(num / 2);
+   }
+
+   return numBinario;
+
+}
 
 module.exports = {
    BinarioADecimal,
